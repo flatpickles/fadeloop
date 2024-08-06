21f23fd79ff640feca39b82e1af403df0cda7171
Matt Nichols <mattnichols206@gmail.com>
2024-08-05 21:51:44 -0700

    flip & fade audio track as well

--- crossfade.sh
+++ crossfade.sh
@@ -25,11 +25,16 @@ fi
 # Generate output filename
 output_file="${input_file%.*}_reversed_end_crossfaded.mp4"
 
-# Perform the video editing
+# Perform the video and audio editing
 ffmpeg -i "$input_file" -filter_complex "
-[0:v]trim=0:${cut_point},setpts=PTS-STARTPTS[first];
-[0:v]trim=${cut_point}:${new_length},setpts=PTS-STARTPTS[second];
-[second][first]xfade=transition=fade:duration=${crossfade_duration}:offset=0,setpts=PTS-STARTPTS
-" -t ${new_length} -c:a copy "$output_file"
+[0:v]split[v1][v2];
+[v1]trim=0:${cut_point},setpts=PTS-STARTPTS[vfirst];
+[v2]trim=${cut_point}:${new_length},setpts=PTS-STARTPTS[vsecond];
+[vsecond][vfirst]xfade=transition=fade:duration=${crossfade_duration}:offset=0,setpts=PTS-STARTPTS[vout];
+[0:a]asplit[a1][a2];
+[a1]atrim=0:${cut_point},asetpts=PTS-STARTPTS[afirst];
+[a2]atrim=${cut_point}:${new_length},asetpts=PTS-STARTPTS[asecond];
+[asecond][afirst]acrossfade=d=${crossfade_duration}:c1=tri:c2=tri[aout]
+" -map "[vout]" -map "[aout]" -t ${new_length} "$output_file"
 
 echo "Processed video saved as $output_file"
